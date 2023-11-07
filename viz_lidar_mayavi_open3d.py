ea0f8d0c8c3001cdcd899031c57488e00063d6a2
sijieaaa <58644619+sijieaaa@users.noreply.github.com>
2023-11-07 19:21:16 +0800

    Update viz_lidar_mayavi_open3d.py

--- viz_lidar_mayavi_open3d.py
+++ viz_lidar_mayavi_open3d.py
@@ -6,34 +6,57 @@ import numpy as np
 import open3d as o3d
 
 
+c = 0.8
+# RGB
+cmaps = [
+    [0, 0, c],
+    [0, c, 0],
+    [0, c, c],
+    [c, 0, 0],
+    [c, 0, c],
+    [c, c, 0],
+    [c, c, c],
+]
 
-def viz_lidar_open3d(pointcloud, colors=None, width=None, height=None):
+
+def viz_lidar_open3d(pointcloud, width=None, height=None):
 
 
 
     FOR1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=10, origin=[0, 0, 0])
 
 
-    x = pointcloud[:,0]  # x position of point
-    y = pointcloud[:,1]  # y position of point
-    z = pointcloud[:,2]  # z position of point
+    pcd_list = [FOR1]
 
-    pcd = open3d.geometry.PointCloud()
-    points = np.hstack([x[:,None],y[:,None],z[:,None]])
-    points = open3d.utility.Vector3dVector(points)
-    pcd.points = points
-
-
-    if colors is not None:
+    if isinstance(pointcloud, np.ndarray):
+        pcd = open3d.geometry.PointCloud()
+        points = pointcloud
+        points = open3d.utility.Vector3dVector(points)
+        pcd.points = points
+        colors = np.ones([pointcloud.shape[0], 3]) * cmaps[0]
         pcd.colors = open3d.utility.Vector3dVector(colors)
-    
+        pcd_list.append(pcd)
+
+    elif isinstance(pointcloud, list):
+        for i in range(len(pointcloud)): 
+            pcd = open3d.geometry.PointCloud()
+            points = pointcloud[i]
+            points = open3d.utility.Vector3dVector(points)
+            pcd.points = points
+            colors = np.ones([pointcloud[i].shape[0], 3]) * cmaps[i]
+            pcd.colors = open3d.utility.Vector3dVector(colors)
+            pcd_list.append(pcd)
+
+    else:
+        raise NotImplementedError
+
+
 
 
 
     if (width is not None) & (height is not None):
-        open3d.visualization.draw_geometries([pcd,FOR1], width=width, height=height)
+        open3d.visualization.draw_geometries(pcd_list, width=width, height=height)
     else:
-
-        open3d.visualization.draw_geometries([pcd,FOR1])
+        open3d.visualization.draw_geometries(pcd_list)
 
 
