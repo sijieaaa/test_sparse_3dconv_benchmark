b8ec74f380b53a047564a35799b0a60113381d22
sijieaaa <58644619+sijieaaa@users.noreply.github.com>
2024-11-07 21:54:03 +0800

    Update viz_lidarv2.py

--- viz_lidarv2.py
+++ viz_lidarv2.py
@@ -4,7 +4,7 @@ import open3d
 import numpy as np
 import open3d as o3d
 from scipy.spatial.transform import Rotation
-
+from open3d.visualization import gui
 # c = 0.8
 # # RGB
 # cmaps = [
@@ -16,9 +16,12 @@ from scipy.spatial.transform import Rotation
 #     [c, c, 0],
 #     [c, c, c],
 # ]
+num_classes = 256
+cmap = np.random.rand(num_classes, 3)
+cmap[0] = [0, 0, 0]
 
 def viz_lidar_open3dv2(posest=None, posesT=None, width=None, height=None, return_pcd_list=False,
-                       colors=None):
+                       colors=None, label_names=None, labelids=None):
     '''
     posest: [n,3]. Contains 3D points (or translation vectors), no rotation.
     posesT: [n,4,4] or [n,3,4]. Contains 3x3 rotation matrix and 3x1 translation vector.
@@ -35,6 +38,8 @@ def viz_lidar_open3dv2(posest=None, posesT=None, width=None, height=None, return
         pcd.points = o3d.utility.Vector3dVector(posest)
         if colors is not None:
             pcd.colors = o3d.utility.Vector3dVector(colors)
+        if labelids is not None:
+            pcd.colors = o3d.utility.Vector3dVector(cmap[labelids])
         pcd_list.append(pcd)
 
     if posesT is not None:
@@ -51,7 +56,22 @@ def viz_lidar_open3dv2(posest=None, posesT=None, width=None, height=None, return
         return pcd_list
     
     o3d.visualization.draw_geometries(pcd_list)
-        
+
+
+
+    # app = gui.Application.instance
+    # app.initialize()
+    # pcd = pcd_list[1]
+    # vis = o3d.visualization.O3DVisualizer("Open3D - 3D Text", 1024, 768)
+    # vis.show_settings = True
+    # vis.add_geometry("Points", pcd)
+    # for i in range(0, len(pcd.points)):
+    #     vis.add_3d_label(pcd.points[i], f'{label_names[i]}')
+    # vis.reset_camera_to_default()
+    # app.add_window(vis)
+    # app.run()
+
+
 
 
 if __name__ == '__main__':
